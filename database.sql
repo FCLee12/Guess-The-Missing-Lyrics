4fa8d29ad51f3ca9f90d5ca6e3ff59659ad1765e
Fue Lee <fueclee.12@gmail.com>
2023-04-17 11:17:20 -0500

    wrote SQL to generate tables with expected information

--- database.sql
+++ database.sql
@@ -6,5 +6,18 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email_address" VARCHAR (250) NOT NULL,
+    "game_id" INT
+);
+
+CREATE TABLE "gameSongs" (
+    "id" SERIAL PRIMARY KEY,
+    "title" VARCHAR (250) UNIQUE NOT NULL,
+    "artist" VARCHAR (250) NOT NULL,
+    "edited_lyrics" VARCHAR (1000) NOT NULL,
+    "answer_lyrics" VARCHAR (1000) NOT NULL,
+    "status" BOOLEAN DEFAULT FALSE,
+    "user_id" INT REFERENCES "user",
+    "score" INT
 );
