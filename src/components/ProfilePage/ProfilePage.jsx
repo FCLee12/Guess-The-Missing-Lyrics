beda4b100775a56eb6e197d61e0c461d40a4d208
Fue Lee <fueclee.12@gmail.com>
2023-04-18 16:51:49 -0500

    built out skeleton of the profile page

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,28 @@
+import { useSelector } from "react-redux";
+
 function ProfilePage() {
+
+    const user = useSelector((store) => store.user);
+
     return(
         <>
-            <h1>This is the Profile Page!</h1>
-            <p>{user.id}</p>
+            <h1>Hello {user.username}</h1>
+            <div>
+                <img src='' alt='user avatar' style={{width: '250px', height: '250px'}}/>
+            </div>
+            <div>
+                <h3>Username:</h3>
+                <p>{user.username}</p>
+                <label>Change your username:</label>
+                <button>Edit</button>
+                <h3>Password:</h3>
+                <label>Change your password:</label>
+                <button>Edit</button>
+                <h3>Email Address:</h3>
+                <p>{user.email_address}</p>
+                <label>Change your eamil address:</label>
+                <button>Edit</button>
+            </div>
         </>
     )
 }
