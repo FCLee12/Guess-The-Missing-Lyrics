62fb0200b9fd002ac7f2a6942846745cc3e05e73
Fue Lee <fueclee.12@gmail.com>
2023-04-19 13:24:44 -0500

    removed UserSongCard component for simplicity sake, working on PUT and GET functionality in user dashboard

--- src/components/UserSongList/UserSongList.jsx
+++ src/components/UserSongList/UserSongList.jsx
@@ -1,11 +1,34 @@
-import SongListCard from "../SongListCard/SongListCard";
+import * as React from 'react';
+import { Card, CardContent, Typography, CardActionArea } from '@mui/material';
 
 function UserSongList() {
-    return(
-        <>
-            <SongListCard />
-        </>
-    )
+    
+    const setActive = (event) => {
+        console.log('setActive is running');
+        // will need a dispatch to SAGA to do a put request to SERVER which will set the status from active = false to active = true
+            // meaning the song will appear when a guest user uses a registered user's gameID
+      }
+    
+      // need a dispatch call to SAGA to do a get request to SERVER/ROUTER who will pull data from DB then store it in a reducer
+    
+    
+      return (
+        <Card sx={{ maxWidth: 300 }}>
+            <CardContent>
+                <CardActionArea onClick={setActive}>
+                    <Typography>
+                        Song Title:
+                    </Typography>
+                    <Typography>
+                        Song Artist:
+                    </Typography>
+                </CardActionArea>
+                <Typography variant="body2" color="text.secondary">
+                    There will be some song lyrics that go here
+                </Typography>
+            </CardContent>
+        </Card>
+      );
 }
 
 export default UserSongList;
