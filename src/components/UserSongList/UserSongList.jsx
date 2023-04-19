28a914882d017ea4c9be48a8e19a830202166972
Fue Lee <fueclee.12@gmail.com>
2023-04-19 16:42:16 -0500

    song cards are displayed properly on the DOM for user-dashboard from GET request

--- src/components/UserSongList/UserSongList.jsx
+++ src/components/UserSongList/UserSongList.jsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 import { Card, CardContent, Typography, CardActionArea } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 function UserSongList() {
+
+    const dispatch = useDispatch();
+    const songList = useSelector(store => store.songsReducer);
+    console.log('this is songList.data', songList.data);
     
     const setActive = (event) => {
         console.log('setActive is running');
@@ -10,25 +17,36 @@ function UserSongList() {
       }
     
       // need a dispatch call to SAGA to do a get request to SERVER/ROUTER who will pull data from DB then store it in a reducer
-      
+      useEffect(() => {
+        dispatch({
+            type: 'FETCH_SONGS'
+        });
+      }, []);
     
       return (
-        <Card sx={{ maxWidth: 300 }}>
-            <CardContent>
-                <CardActionArea onClick={setActive}>
-                    <Typography>
-                        Song Title:
-                    </Typography>
-                    <Typography>
-                        Song Artist:
-                    </Typography>
-                </CardActionArea>
-                <Typography variant="body2" color="text.secondary">
-                    There will be some song lyrics that go here
-                </Typography>
-            </CardContent>
-        </Card>
+        <ul>
+            {songList.data ?
+                songList.data.map((song) => {
+                    return (<Card sx={{ maxWidth: 300 }}>
+                        <CardContent>
+                            <CardActionArea onClick={setActive}>
+                                <Typography>
+                                    Song Title: {song.title}
+                                </Typography>
+                                <Typography>
+                                    Song Artist: {song.artist}
+                                </Typography>
+                            </CardActionArea>
+                            <Typography variant="body2" color="text.secondary">
+                                There will be some song lyrics that go here
+                            </Typography>
+                        </CardContent>
+                    </Card> 
+                )}) : <li><p>Loading</p></li>
+            }
+        </ul>
       );
+      
 }
 
 export default UserSongList;
