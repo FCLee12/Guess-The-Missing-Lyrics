0954f4863dbf9202185c51af3993f7110e14a689
Fue Lee <fueclee.12@gmail.com>
2023-04-19 10:54:14 -0500

    created components for user dashboard

--- src/components/UserDashboard/UserDashboard.jsx
+++ src/components/UserDashboard/UserDashboard.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import UserSongList from '../UserSongList/UserSongList';
 
-function UserPage() {
+function UserDashboard() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
+    <>
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
     </div>
+    <div>
+      <h2>Song List Goes Here</h2>
+      <UserSongList />
+    </div>
+    </>
   );
 }
 
 // this allows us to use <App /> in index.js
-export default UserPage;
+export default UserDashboard;
---
+++ src/components/UserSongList/UserSongList.jsx
@@ -0,0 +1,11 @@
+import SongListCard from "../SongListCard/SongListCard";
+
+function UserSongList() {
+    return(
+        <>
+            <SongListCard />
+        </>
+    )
+}
+
+export default UserSongList;
