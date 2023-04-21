a1719af8f37656562cef663afe69cc5ad21d6188
Fue Lee <fueclee.12@gmail.com>
2023-04-20 20:59:15 -0500

    delete button and delete modal to verify delete functions but delete only seems to work on last item in list of items no matter which item's delete button you click on

--- src/components/UserSongList/UserSongList.jsx
+++ src/components/UserSongList/UserSongList.jsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { Card, CardContent, Typography, CardActionArea, Grid } from '@mui/material';
+import { Box, Button, Card, CardContent, Typography, CardActionArea, Stack, Modal, Grid } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import  DeleteIcon from '@mui/icons-material/Delete';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import EditNoteIcon from '@mui/icons-material/EditNote';
-import { Button, Stack } from '@mui/material/';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 function UserSongList() {
 
@@ -30,9 +30,28 @@ function UserSongList() {
     const handleDelete = (id) => {
         console.log('in handleDelete, see id', id);
         dispatch({
-            type: 'DELETE_SONG'
+            type: 'DELETE_SONG',
+            payload: id
         })
     }
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 240,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+        align: 'center'
+      };
+    
+    // For the DELETE MODAL
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     
     return (
     <>
@@ -67,12 +86,41 @@ function UserSongList() {
                                 endIcon={<DeleteIcon />}
                                 size="small"
                                 color="error"
-                                onClick={() => handleDelete(song.id)}>
+                                onClick={handleOpen}>
                                 Delete
                             </Button>
+                            <Modal
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description">
+                            <Box sx={style}>
+                                <Typography id="modal-modal-title" variant="h6" component="h2">
+                                    Are you sure you want to delete this song?
+                                </Typography>
+                                <Grid container sx={{justifyContent: 'space-between', maxWidth: 240}}>
+                                    <Button
+                                    variant="contained" 
+                                    endIcon={<DeleteIcon />}
+                                    size="small"
+                                    color="error"
+                                    onClick={() => handleDelete(song.id)}>
+                                    Delete
+                                    </Button>
+                                    <Button
+                                    variant="outlined" 
+                                    endIcon={<CancelIcon />}
+                                    size="small"
+                                    color="info"
+                                    onClick={handleClose}>
+                                    Cancel
+                                    </Button>
+                                </Grid>
+                            </Box>
+                            </Modal>
                         </Stack>
                     </CardContent>
-                </Card> 
+                </Card>
             )}) : <li><p>Loading</p></li>
         }
     </>
