143624758d32d509cd1e9b07d9714eebd69bc07c
Fue Lee <fueclee.12@gmail.com>
2023-04-24 19:56:28 -0500

    created a floating box for users to enter in their guesses

--- src/components/PlayPage/PlayPage.jsx
+++ src/components/PlayPage/PlayPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button, Grid, Typography, TextField, Paper } from '@mui/material/';
+import { Button, Card, Grid, Typography, TextField, Paper } from '@mui/material/';
 import SendIcon from '@mui/icons-material/Send';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
@@ -12,14 +12,39 @@ function PlayPage() {
   let songObj = activeSong.activeSongReducer;
   // console.log('this is songObj taken from the store in PlayPage', songObj);
   
+  const style = {
+    position: 'fixed',
+    top: 580,
+    left: 160.5,
+    transform: 'translate(-50%, -50%)',
+    width: 234.5,
+    height: 100,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    flexDirection:"column",
+    textAlign:"center"
+  };
   
   return (
     <>
-      <Paper>
-        <Typography>
-
+      <Grid container sx={{flexDirection: "column", ml: 1.2, mb: 23}}>
+        <Typography variant='h5' align='center' sx={{padding: 1, width: 300, ml: -1}}>
+          {songObj.title}
         </Typography>
-      </Paper>
+        <Typography variant='h6' align='center' sx={{padding: 1, width: 300, ml: -1}}>
+          By: {songObj.artist}
+        </Typography>
+        <Paper sx={{width: 300, border: 'solid black 1px'}}>
+          <Typography align='center' sx={{padding: 1}}>
+            {songObj.edited_lyrics}
+          </Typography>
+        </Paper>
+        <Card sx={style}>
+          <Typography>This is some text</Typography>
+        </Card>
+      </Grid>
     </>
   );
 }
