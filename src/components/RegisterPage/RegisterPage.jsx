4e40970d67a3a0aaa56093d2f89caaa8fa1f41bd
Fue Lee <fueclee.12@gmail.com>
2023-04-27 10:00:14 -0500

    general styling completed

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 
 import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
+import { Card } from '@mui/material/';
 
 function RegisterPage() {
   const history = useHistory();
 
+  const cardStyle = {
+    flexDirection:"column",
+    marginLeft: 1.2,
+    textAlign:"center",
+    width: 300,
+    border: 'solid black 1px'
+  }
+
   return (
     <>
-      <div className='container'>
+      <Card sx={cardStyle}>
         <RegisterForm />
-      </div>
+      </Card>
     </>
   );
 }
